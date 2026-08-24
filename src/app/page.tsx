0da9311a620c0bd2928a1f957b1b036ae91e02f7
ArJun-012 <arjundasofficial012@gmail.com>
2026-08-24 23:29:08 +0530

    hid the pricing section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <Work />
       <Services />
       <Testimonials />
-      <Pricing />
+      {/* <Pricing /> */}
       <FAQ />
       <BlogPreview />
     </>
